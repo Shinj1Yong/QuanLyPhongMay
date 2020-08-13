a78855ca077682b43b9e9594c95851bd14f1cb81
ShinJi Dev <64673239+Shinj1Yong@users.noreply.github.com>
2020-08-13 23:02:01 +0700

    UpdateDAL2

---
+++ QuanLyPhongMay/QuanLyPhongNet.DAL/NhomNguoiDungDAL.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using QuanLyPhongNet.DTO;
+namespace QuanLyPhongNet.DAL
+{
+    public class NhomNguoiDungDAL
+    {
+        QuanLyPhongNetDataContext qlpn = new QuanLyPhongNetDataContext();
+        public NhomNguoiDungDAL()
+        {
+
+        }
+        public List<QuanLyPhongNet.DTO.GroupUser> getNhomNguoiDung()
+        {
+            return (from kh in qlpn.GroupUsers
+                    select new QuanLyPhongNet.DTO.GroupUser
+                    {
+                        GroupUserName = kh.GroupName,
+                        TypeName = kh.TypeName
+                    }
+                    ).ToList();
+        }
+    }
+}
